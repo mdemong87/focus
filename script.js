aeb0bb17dde06dca39f128b36052254af41568c3
Md Emon Hossen <mdemong87@gmail.com>
2023-04-26 10:27:29 +0600

    update

---
+++ script.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+    autoplay: {
+        delay: 2000,
+    },
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
+
+
