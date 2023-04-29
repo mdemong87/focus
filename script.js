3cabba48ad2b3601cd01e50ed90d6a9c5ab73f6b
Md Emon Hossen <mdemong87@gmail.com>
2023-04-29 12:14:40 +0600

    update

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    // direction: 'vertical',
+const swiper = new Swiper('.letestSwiper', {
     loop: true,
     autoplay: {
         delay: 2000,
+        disableOnInteraction: false,
     },
     // If we need pagination
     pagination: {
-        el: '.swiper-pagination',
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        400: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+        },
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
     },
 });
 
 
+// const swiper = new Swiper('.cliectFee', {
+//     loop: true,
+//     autoplay: {
+//         delay: 2000,
+//         disableOnInteraction: false,
+//     },
+//     // If we need pagination
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//     },
+// });
